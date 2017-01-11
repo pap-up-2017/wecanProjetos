3e37c0ff83b98ed31f4c2c17dc2f8ade2a271ab7
EDUARDO-PC\ChaOa <chaoak@outlook.com>
2017-01-11 00:09:15 -0200

    Atualização da classe de usuário

--- projeto/ProjetoPAP/src/projeto/entity/Usuario.java
+++ projeto/ProjetoPAP/src/projeto/entity/Usuario.java
@@ -1,5 +1,17 @@
 package projeto.entity;
 
 public class Usuario {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 
 }
