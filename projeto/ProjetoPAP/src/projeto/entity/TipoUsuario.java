691bf04eee028d781bec71832b039050ad9586c2
ghtoporowicz <gh.toporowicz@gmail.com>
2017-01-12 21:25:44 -0200

    atualização da classe tipoUsuario

--- projeto/ProjetoPAP/src/projeto/entity/TipoUsuario.java
+++ projeto/ProjetoPAP/src/projeto/entity/TipoUsuario.java
@@ -1,6 +1,15 @@
 package projeto.entity;
 
 public class TipoUsuario {
+	private int idTipoUsuario;
+	public int getIdTipoUsuario() {
+		return idTipoUsuario;
+	}
+
+	public void setIdTipoUsuario(int idTipoUsuario) {
+		this.idTipoUsuario = idTipoUsuario;
+	}
+
 	private String tipoUsuario;
 
 	public String getTipoUsuario() {
