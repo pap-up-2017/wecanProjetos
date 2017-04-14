1d1937827f3929c232dd178fa15936ff1d25489e
ghtoporowicz <gh.toporowicz@gmail.com>
2017-04-14 13:53:28 -0300

    Inserção do Layout na pagina home com a função login

--- projeto/ProjetoPAP/WebContent/js/ancora.js
+++ projeto/ProjetoPAP/WebContent/js/ancora.js
@@ -1,25 +1,16 @@
-$(document).ready(function () {
-    function filterPath(string) {
-        return string
-            .replace(/^\//, '')
-            .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
-            .replace(/\/$/, '');
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
     }
-    $('a[href*=#]').each(function () {
-        if (filterPath(location.pathname) == filterPath(this.pathname)
-                && location.hostname == this.hostname
-                && this.hash.replace(/#/, '')) {
-            var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
-            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-            if ($target) {
-                var targetOffset = $target.offset().top;
-                $(this).click(function () {
-                    $('html, body').animate({scrollTop: targetOffset}, 300);
-                    return false;
-                });
-            }
-        }
-    });
+  });
 });
-			// Outras Animações
+			// Outras AnimaÃ§Ãµes
 			// linear, swing, jswing, easeInQuad, easeInCubic, easeInQuart, easeInQuint, easeInSine, easeInExpo, easeInCirc, easeInElastic, easeInBack, easeInBounce, easeOutQuad, easeOutCubic, easeOutQuart, easeOutQuint, easeOutSine, easeOutExpo, easeOutCirc, easeOutElastic, easeOutBack, easeOutBounce, easeInOutQuad, easeInOutCubic, easeInOutQuart, easeInOutQuint, easeInOutSine, easeInOutExpo, easeInOutCirc, easeInOutElastic, easeInOutBack, easeInOutBounce
---
+++ projeto/ProjetoPAP/WebContent/js/principal.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+// mostra esconde menu
+  $("a.mostra-esconde-menu").click(function(){
+      $(".menu_mluv_retratil").toggleClass('esconder');
+
+      if ($(this).text() != "Perfil ▼")
+       $(this).text("Perfil ▼")
+        else
+       $(this).text("Perfil ▲");      
+  }); 
+});
