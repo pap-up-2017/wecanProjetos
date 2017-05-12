3c8e71239a1f3d51f228935d4de859ba26e2ca1c
ghtoporowicz <gh.toporowicz@gmail.com>
2017-05-11 22:18:39 -0300

    melhoria para iniciar o banco...

--- projeto/ProjetoPAP/src/projeto/test/TestIniciaBanco.java
+++ projeto/ProjetoPAP/src/projeto/test/TestIniciaBanco.java
@@ -2,50 +2,70 @@ package projeto.test;
 
 import org.junit.Test;
 
+import projeto.business.UsuarioBusiness;
+import projeto.dao.CidadeDao;
+import projeto.dao.CursoDao;
+import projeto.dao.EstadoDao;
 import projeto.dao.FactoryDao;
+import projeto.dao.InstituicaoEnsinoDao;
 import projeto.dao.InterfaceDao;
+import projeto.dao.TipoUsuarioDao;
 import projeto.entity.Cidade;
 import projeto.entity.Curso;
 import projeto.entity.Estado;
 import projeto.entity.InstituicaoEnsino;
 import projeto.entity.TipoUsuario;
+import projeto.entity.Usuario;
 
 public class TestIniciaBanco {
 	
 	@Test
 	public void cadastrarUsuarioAdmin(){
-		// cad estado
-		InterfaceDao<Estado> daoUf = FactoryDao.createEstadoDao();
-		Estado uf = new Estado();
-		uf.setNomeEstado("Paraná");
-		uf.setSiglaEstado("PR");
-		daoUf.salvar(uf);
-		
-		// cad cidade
-		InterfaceDao<Cidade> daoCidade = FactoryDao.createCidadeDao();
-		Cidade c = new Cidade();
-		c.setNomeCidade("Curitiba");
-		c.setEstadoCidade(daoUf.getObjById(1));
-		daoCidade.salvar(c);
-		
-		//cadInstituicao
-		InterfaceDao<InstituicaoEnsino> daoInst = FactoryDao.createInstituicaoEnsinoDao();
-		InstituicaoEnsino i = new InstituicaoEnsino();
-		i.setNomeInstituicao("Universidade Positivo");
-		i.setCidadeInstituicao(daoCidade.getObjById(1));
-		daoInst.salvar(i);
-		
-		//cadCurso
-		InterfaceDao<Curso> daoCurso = FactoryDao.createCursoDao();
-		Curso curso = new Curso();
-		curso.setNomeCurso("Analise e desenvolvimento de Sistemas");
-		curso.setInstituicaoCurso(daoInst.getObjById(1));
-		daoCurso.salvar(curso);
-		
-		//CadTipoUsuario
-		InterfaceDao<TipoUsuario> dao = FactoryDao.createTipoUsuarioDao();
+		// cria tipo usuario 
 		TipoUsuario t = new TipoUsuario();
 		t.setTipoUsuario("Administrador");
-		dao.salvar(t);
+		TipoUsuarioDao tDao = new TipoUsuarioDao();
+		tDao.salvar(t);
+		
+		// cria estado
+		Estado e = new Estado();
+		e.setNomeEstado("Paraná");
+		e.setSiglaEstado("PR");
+		EstadoDao eDao = new EstadoDao();
+		eDao.salvar(e);
+		
+		// cria cidade 
+		Cidade c = new Cidade();
+		c.setNomeCidade("Curitiba");
+		c.setEstadoCidade(eDao.getObjById(1));
+		CidadeDao cDao = new CidadeDao();
+		cDao.salvar(c);
+		
+		// cria instituição
+		InstituicaoEnsino i = new InstituicaoEnsino();
+		i.setNomeInstituicao("Universidade Positivo");
+		i.setCidadeInstituicao(cDao.getObjById(1));
+		InstituicaoEnsinoDao iDao = new InstituicaoEnsinoDao();
+		iDao.salvar(i);
+		
+		//cria curso
+		Curso cs = new Curso();
+		cs.setNomeCurso("Analise e Desenvolvimento de Sistemas");
+		cs.setInstituicaoCurso(iDao.getObjById(1));
+		CursoDao csDao = new CursoDao();
+		csDao.salvar(cs);
+		
+		// cria usuario
+		Usuario u = new Usuario();
+		u.setNomeUsuario("Administrador do Sistema");
+		u.setUsernameUsuario("administrador");
+		u.setEmailUsuario("adm@wecan.com");
+		u.setSenhaUsuario("wecanADM");
+		u.setTipoUsuario(tDao.getObjById(1));
+		u.setCidadeUsuario(cDao.getObjById(1));
+		u.setInstituicaoUsuario(iDao.getObjById(1));
+		u.setCursoUsuario(csDao.getObjById(1));
+		UsuarioBusiness uBusiness = new UsuarioBusiness();
+		uBusiness.create(u);
 	}
 }
